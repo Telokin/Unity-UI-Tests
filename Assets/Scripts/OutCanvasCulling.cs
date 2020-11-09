5a3e64e77d0b8c2a008d8af292f79bd1603ba307
Jakub Furman <44147232+Telokin@users.noreply.github.com>
2020-11-09 17:37:06 +0100

    First commit

---
+++ Assets/Scripts/OutCanvasCulling.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OutCanvasCulling : MonoBehaviour
+{
+    Vector3 pos;
+    float x, y, z;
+    int i = 0;
+    [SerializeField]
+    private Image outCanvas;
+    void Start()
+    {
+        TestCulling2();
+
+    }
+
+    private void TestCulling2()
+    {
+        for(i = 0; i <= 10000; i++)
+        {
+            GameObject canvas = GameObject.Find("Test");
+            Image hit = Instantiate(outCanvas, transform.position, Quaternion.identity);
+            hit.transform.SetParent(canvas.transform, false);
+            x = Random.Range(-1000, -100);
+            y = Random.Range(0, 1000);
+            z = 0;
+            pos = new Vector3(x, y, z);
+            hit.transform.position = pos;
+        }
+        
+        
+    }
+}
---
+++ Assets/Scripts/TestUI.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+//[ExecuteInEditMode]
+public class TestUI : MonoBehaviour
+{
+
+    [SerializeField]
+    private Image inCanvas;
+
+    
+
+    [SerializeField]
+    private TextMeshProUGUI testText;
+
+    Vector3 pos;
+    float x, y, z;
+
+    public Text fpsText;
+    public float deltaTime;
+    int i = 0;
+
+    // Start is called before the first frame update
+
+    void Update()
+    {
+        deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+        float fps = 1.0f / deltaTime;
+        fpsText.text = Mathf.Ceil(fps).ToString();
+
+        
+        Debug.Log(Time.time);
+    }
+
+
+
+    void Start()
+    {
+        //Test();
+        //TestTextMeshPro();
+        //TestCulling();
+        //Debug.Log(Time.realtimeSinceStartup);
+
+    }
+
+    private void Test()
+    {
+        //var tempColor = image.color;
+        //tempColor.a = 0;
+        //image.color = tempColor;
+        //Debug.LogWarning(image);
+    }
+
+    private void TestTextMeshPro()
+    {
+        //GameObject canvas = GameObject.Find("Canvas");
+        //TextMeshProUGUI hit = Instantiate(testText, transform.position, Quaternion.identity);
+        //hit.transform.SetParent(canvas.transform, false);
+        //hit.transform.position = transform.position;
+    }
+
+    private void TestCulling()
+    {
+        GameObject canvas = GameObject.Find("Child");
+        //if (Time.deltaTime <= 10)
+        //{
+
+        //        Image hit = Instantiate(inCanvas, transform.position, Quaternion.identity);
+        //        hit.transform.SetParent(canvas.transform, false);
+        //        x = Random.Range(0, 1000);
+        //        y = Random.Range(0, 400);
+        //        z = 0;
+        //        pos = new Vector3(x, y, z);
+        //        hit.transform.position = pos;
+
+
+        //}
+
+        for (i = 0; i <= 500; i++)
+        {
+            Image hit = Instantiate(inCanvas, transform.position, Quaternion.identity);
+            hit.transform.SetParent(canvas.transform, false);
+            x = Random.Range(0, 1000);
+            y = Random.Range(0, 400);
+            z = 0;
+            pos = new Vector3(x, y, z);
+            hit.transform.position = pos;
+        }
+
+    }
+
+    
+
+
+
+}
